a1f153bb0199d2279bf47481f7e655e874bf79ab
bubbaboogs <73847191+bubbaboogs@users.noreply.github.com>
2026-07-23 15:50:16 -0400

    change rules a little

--- js/pages/List.js
+++ js/pages/List.js
@@ -149,7 +149,7 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Any submitted levels must be >60 seconds long, have >1000 objects, and cannot be a derivative of any other level (no buffed levels or other edits). Exceptions are "shplork" versions of levels.
+                        Any submitted levels must be rated levels. Exceptions are "shplork" versions of levels.
                     </p>
                     <p>
                         Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
