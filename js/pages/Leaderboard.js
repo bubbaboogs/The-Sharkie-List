6ae297e395658a13b8dc38debf8505e18ef713c7
bubbaboogs <73847191+bubbaboogs@users.noreply.github.com>
2026-08-23 15:03:36 -0400

    undo

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -1,6 +1,5 @@
 import { fetchLeaderboard } from '../content.js';
 import { localize } from '../util.js';
-import { get_gsv_profile } from '../api.js';
 
 import Spinner from '../components/Spinner.js';
 
@@ -8,39 +7,32 @@ export default {
     components: {
         Spinner,
     },
-
     data: () => ({
         leaderboard: [],
         loading: true,
         selected: 0,
         err: [],
     }),
-
     template: `
         <main v-if="loading">
             <Spinner></Spinner>
         </main>
-
         <main v-else class="page-leaderboard-container">
             <div class="page-leaderboard">
-
                 <div class="error-container">
                     <p class="error" v-if="err.length > 0">
                         Leaderboard may be incorrect, as the following levels could not be loaded: {{ err.join(', ') }}
                     </p>
                 </div>
-
                 <div class="board-container">
                     <table class="board">
                         <tr v-for="(ientry, i) in leaderboard">
                             <td class="rank">
                                 <p class="type-label-lg">#{{ i + 1 }}</p>
                             </td>
-
                             <td class="total">
                                 <p class="type-label-lg">{{ localize(ientry.total) }}</p>
                             </td>
-
                             <td class="user" :class="{ 'active': selected == i }">
                                 <button @click="selected = i">
                                     <span class="type-label-lg">{{ ientry.user }}</span>
@@ -49,90 +41,47 @@ export default {
                         </tr>
                     </table>
                 </div>
-
                 <div class="player-container">
                     <div class="player">
-                        <h1>
-                            #{{ selected + 1 }}
-                            <a href="javascript:void(0)" @click.prevent="profileLink(entry.user)">
-                                {{ entry.user }}
-                            </a>
-                        </h1>
-
+                        <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
                         <h3>{{ entry.total }}</h3>
-
-                        <h2 v-if="entry.verified.length > 0">
-                            Verified ({{ entry.verified.length }})
-                        </h2>
-
+                        <h2 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h2>
                         <table class="table">
                             <tr v-for="score in entry.verified">
                                 <td class="rank">
                                     <p>#{{ score.rank }}</p>
                                 </td>
-
                                 <td class="level">
-                                    <a
-                                        class="type-label-lg"
-                                        target="_blank"
-                                        :href="score.link"
-                                    >
-                                        {{ score.level }}
-                                    </a>
+                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
                                 </td>
-
                                 <td class="score">
                                     <p>+{{ localize(score.score) }}</p>
                                 </td>
                             </tr>
                         </table>
-
-                        <h2 v-if="entry.completed.length > 0">
-                            Completed ({{ entry.completed.length }})
-                        </h2>
-
+                        <h2 v-if="entry.completed.length > 0">Completed ({{ entry.completed.length }})</h2>
                         <table class="table">
                             <tr v-for="score in entry.completed">
                                 <td class="rank">
                                     <p>#{{ score.rank }}</p>
                                 </td>
-
                                 <td class="level">
-                                    <a
-                                        class="type-label-lg"
-                                        target="_blank"
-                                        :href="score.link"
-                                    >
-                                        {{ score.level }}
-                                    </a>
+                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
                                 </td>
-
                                 <td class="score">
                                     <p>+{{ localize(score.score) }}</p>
                                 </td>
                             </tr>
                         </table>
-
-                        <h2 v-if="entry.progressed.length > 0">
-                            Progressed ({{ entry.progressed.length }})
-                        </h2>
-
+                        <h2 v-if="entry.progressed.length > 0">Progressed ({{entry.progressed.length}})</h2>
                         <table class="table">
                             <tr v-for="score in entry.progressed">
                                 <td class="rank">
                                     <p>#{{ score.rank }}</p>
                                 </td>
-
                                 <td class="level">
-                                    <a
-                                        class="type-label-lg"
-                                        target="_blank"
-                                        :href="score.link"
-                                    >
-                                        {{ score.percent }}% {{ score.level }}
-                                    </a>
+                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.percent }}% {{ score.level }}</a>
                                 </td>
-
                                 <td class="score">
                                     <p>+{{ localize(score.score) }}</p>
                                 </td>
@@ -140,38 +89,22 @@ export default {
                         </table>
                     </div>
                 </div>
-
             </div>
         </main>
     `,
-
     computed: {
         entry() {
             return this.leaderboard[this.selected];
         },
     },
-
     async mounted() {
         const [leaderboard, err] = await fetchLeaderboard();
-
         this.leaderboard = leaderboard;
         this.err = err;
-
+        // Hide loading spinner
         this.loading = false;
     },
-
     methods: {
         localize,
-    
-        async profileLink(username) {
-            const profile_id = await get_gsv_profile(username);
-        
-            if (profile_id) {
-                window.open(
-                    `https://globalstatsviewer.com/users/${profile_id}`,
-                    '_blank'
-                );
-            }
-        },
     },
 };
